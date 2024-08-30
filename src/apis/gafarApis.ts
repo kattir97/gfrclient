3b4ecadb18c08ab278e30fc0dafbc0b83d1a8a41
kattir97 <110621835+kattir97@users.noreply.github.com>
2024-08-31 01:10:18 +0300

    Fixes (#2)
    * add env files
    
    * fix marking
    
    * add nv var
    
    * fix creds, add name, change marker bg, change card styles

--- src/apis/gafarApis.ts
+++ src/apis/gafarApis.ts
@@ -4,11 +4,12 @@ const authToken = localStorage.getItem('authToken');
 
 export const gafarApi = axios.create({
   // baseURL: 'http://localhost:5000/api/v1/',
-  baseURL: 'https://kattir97-gfrserver-8bf8.twc1.net/api/v1/',
+  // baseURL: 'https://kattir97-gfrserver-8bf8.twc1.net/api/v1/',
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     Authorization: `Bearer ${authToken}`
   },
-  withCredentials: true
+  withCredentials: import.meta.env.VITE_WITH_CREDENTIALS === 'true'
 });
 
 // Add a request interceptor
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,30 +1,48 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useAppStore } from "../stores/appStore";
 import { IoIosLogOut } from "react-icons/io";
+import { Dropdown, Space } from "antd";
+import { FaAngleDown } from "react-icons/fa6";
+import type { MenuProps } from "antd";
+import { useAuthStore } from "../stores/authStore";
 
 export const Layout: React.FC = () => {
   const { isLogged, setIsLogged } = useAppStore();
+  const { user } = useAuthStore();
+  console.log("user", user);
   const navigate = useNavigate();
   const handleLogout = () => {
     setIsLogged(false);
     localStorage.removeItem("authToken");
     navigate("/login");
   };
+
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      danger: true,
+      icon: <IoIosLogOut />,
+      label: <a onClick={handleLogout}>Выйти</a>,
+    },
+  ];
   return (
     <>
       <div className="h-12 border-b  flex justify-between items-center px-5">
         <Link to={"/home"}>
           <span className="text-xl font-bold ">Gafar</span>
         </Link>
-        {isLogged ? (
-          <div
-            className="flex gap-2 items-center justify-center  text-blue-600 hover:text-blue-400 cursor-pointer"
-            onClick={handleLogout}
-          >
-            <h3 className="text-xl mb-1"> выйти</h3>
-            <IoIosLogOut className="text-2xl" />
-          </div>
-        ) : null}
+        <div className="mr-5">
+          {isLogged ? (
+            <Dropdown menu={{ items }} placement="bottom">
+              <a onClick={(e) => e.preventDefault()} className="cursor-pointer">
+                <Space className="flex justify-center items-center">
+                  {user}
+                  <FaAngleDown fontSize={15} style={{ marginTop: "3px" }} />
+                </Space>
+              </a>
+            </Dropdown>
+          ) : null}
+        </div>
       </div>
       <div className="flex justify-center py-5  min-h-[90vh]">
         <div className=" lg:w-2/3 my-0 mx-auto border-1 border-inherit border-solid rounded">
